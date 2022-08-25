aa16754911c33301d2e99410f2239fedf6b42421
viktor <manuel66915@gmail.com>
2022-08-24 18:59:58 -0600

    Agrego modelo Alumno

---
+++ src/modelo/Alumno.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import java.util.Date;
+
+public class Alumno {
+	private long id_alumno;
+	private String identificador;
+	private String nombre;
+	private String apellido;
+	private Date fechaNac;
+	public long getId_alumno() {
+		return id_alumno;
+	}
+	public void setId_alumno(long id_alumno) {
+		this.id_alumno = id_alumno;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	@Override
+	public String toString() {
+		return "Alumno [id_alumno=" + id_alumno + ", identificador=" + identificador + ", nombre=" + nombre
+				+ ", apellido=" + apellido + ", fechaNac=" + fechaNac + "]";
+	}
+	
+	
+}
