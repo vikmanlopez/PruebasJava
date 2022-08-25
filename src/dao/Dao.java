7149bd475781125b3ccbaa4d0c0a77deb31e0840
viktor <manuel66915@gmail.com>
2022-08-24 20:23:18 -0600

    Creo la interfaz generica para los Dao

---
+++ src/dao/Dao.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.util.List;
+// generico
+public interface Dao <T,K>{ // T es cualquier objeto y K es la llave primaria
+	public int insertar(T obj);
+	public int modificar(T obj);
+	public int eliminar(T obj);
+	public List<T> listar();
+	public T obtenerPorId(K id);
+	public T obtenerPorIdentificacion(String id);
+	
+
+}
+
+/* Asi seria lo especifico, pero si se neceitan muchos objetos de acceso a datos seria uno por entidad
+ public interface Dao { // T es cualquier objeto y K es la llave primaria
+	public int insertar(Alumno obj);
+	public int modificar(Alumno obj);
+	public int eliminar(Alumno obj);
+	public List<Alumno> listar();
+	public Alumno obtenerPorId(int id);
+	public Alumno obtenerPorIdentificacion(String id);
+	
+}
+ * */
