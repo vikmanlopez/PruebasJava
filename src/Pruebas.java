8d038057bb2703a2a5ae73d6572937bcc43c6fae
viktor <manuel66915@gmail.com>
2022-08-25 07:36:27 -0600

    Prueba de coneccion a la BD

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,9 +1,19 @@
+import java.sql.Connection;
+
+import coneccion.Coneccion;
 
 public class Pruebas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hola mundo");
+		Connection con = null;
+		
+		con = Coneccion.conectar();
+		System.out.println("Conectado a la BD");
+		Coneccion.desconectar(con);
+		System.out.println("Desconectado de la BD");
+		
 	}
 
 }
