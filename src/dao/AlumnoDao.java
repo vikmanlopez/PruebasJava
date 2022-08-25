7bda7fbd7ff4993f903ca36985235d3cdd731859
viktor <manuel66915@gmail.com>
2022-08-24 20:34:12 -0600

    Agrego la implementacion de la interfaz

---
+++ src/dao/AlumnoDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+import modelo.Alumno;
+// una interfaz puede heredar de otra interfaz
+public interface AlumnoDao extends Dao<Alumno, Long>{
+
+}
---
+++ src/daoImpl/AlumnoDaoImpl.java
@@ -0,0 +1,46 @@
+package daoImpl;
+
+import java.util.List;
+
+import dao.AlumnoDao;
+import modelo.Alumno;
+
+public class AlumnoDaoImpl implements AlumnoDao{
+
+	@Override
+	public int insertar(Alumno obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modificar(Alumno obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int eliminar(Alumno obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Alumno> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Alumno obtenerPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Alumno obtenerPorIdentificacion(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
