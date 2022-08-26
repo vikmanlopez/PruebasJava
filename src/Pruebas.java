8f89d90394b722355cb572b17e0a0bd590521b18
viktor <manuel66915@gmail.com>
2022-08-25 20:22:53 -0600

    Metodo para insertar un alumno y prueba

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,12 +1,17 @@
 import java.sql.Connection;
+import java.util.Date;
 
 import coneccion.Coneccion;
+import dao.AlumnoDao;
+import daoImpl.AlumnoDaoImpl;
+import modelo.Alumno;
 
 public class Pruebas {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Hola mundo");
+		// -- Prueba de coneccion------------------------
 		Connection con = null;
 		
 		con = Coneccion.conectar();
@@ -14,6 +19,19 @@ public class Pruebas {
 		Coneccion.desconectar(con);
 		System.out.println("Desconectado de la BD");
 		
+		// --- Prueba de insercion-------------------------
+		AlumnoDao dao = new AlumnoDaoImpl();
+		
+		Alumno a = new Alumno();
+		a.setNombre("Juan");
+		a.setApellido("Perez");
+		a.setIdentificador("AA000");		
+		a.setFechaNac(new Date());
+		
+		int resultado = dao.insertar(a);
+		if(resultado == 1) {
+			System.out.println("El alumno fue insertado exitosamente");
+		}
 	}
 
 }
--- src/daoImpl/AlumnoDaoImpl.java
+++ src/daoImpl/AlumnoDaoImpl.java
@@ -1,7 +1,11 @@
 package daoImpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import coneccion.Coneccion;
 import dao.AlumnoDao;
 import modelo.Alumno;
 
@@ -9,8 +13,29 @@ public class AlumnoDaoImpl implements AlumnoDao{
 
 	@Override
 	public int insertar(Alumno obj) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql="INSERT INTO alumno(nombre, apellido, identificador, fechaNac) VALUES (?,?,?,?)";
+		
+		Connection con = null;
+		PreparedStatement pstm = null;
+		int resultado = 0;
+		
+		con = Coneccion.conectar();
+		
+	    try {
+			pstm = con.prepareStatement(sql);
+			//parametros
+			pstm.setString(1, obj.getNombre());
+			pstm.setString(2, obj.getApellido());
+			pstm.setString(3, obj.getIdentificador());
+			pstm.setDate(4, new java.sql.Date(obj.getFechaNac().getTime()));
+			
+			
+			resultado = pstm.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Error al ejecutar query de insert : "+ e.getMessage());
+		}
+		
+		return resultado;
 	}
 
 	@Override
